09bd7d1b14b17fe1a9ef01f6561f71289807b506
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2023-10-11 09:58:01 -0700

    Add files via upload

---
+++ Weather/Weather/Views/Main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Delstun McCray on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Delstun McCray on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
